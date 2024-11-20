d05aaf1c109b6f31ca213545611eb556d43397cd
Ivaylo Stoyanov <ipstoyanov20@codingburgas.bg>
2024-11-20 22:24:28 +0200

    Remove unneeded code

--- client/components/navbar.tsx
+++ client/components/navbar.tsx
@@ -4,7 +4,7 @@ function Navbar() {
 		<nav className="flex items-center justify-between px-6 py-4 bg-transparent text-white">
 			<div className="w-full h-full grid place-content-center place-items-center absolute">
       <div className="relative grid grid-rows-1 h-8 w-8 rounded-full after:absolute after:text-white after:ml-10 after:z-20 after:content-['Shop'] before:absolute before:text-white before:-left-14 after:top-1 before:top-1 before:z-20 before:content-['Barber']">
-					<img src="/favicon.ico" alt="Logo" className="h-8 w-8 rounded-full mr-10" />
+					<img src="/favicon.ico" alt="Logo" className="h-8 w-8 rounded-full " />
 				</div>
 			</div>
 			<div className="flex items-center space-x-6">
