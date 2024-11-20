d71cc441bf7c25670fcf59f2f66996244cf9263b
Ivaylo Stoyanov <ipstoyanov20@codingburgas.bg>
2024-11-20 12:58:55 +0200

    Return http

--- server/src/BarberApp/BA.Application/Program.cs
+++ server/src/BarberApp/BA.Application/Program.cs
@@ -60,10 +60,13 @@ public class Program
 
 
         // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
+        if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
         {
             app.UseSwagger();
-            app.UseSwaggerUI();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "BA.Application v1");
+            });
         }
 
         app.MapIdentityApi<User>();
