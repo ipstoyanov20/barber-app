b884999689a266909e6995157f23ecc60fc4a1f8
Ivaylo Stoyanov <ipstoyanov20@codingburgas.bg>
2024-11-28 21:57:54 +0200

    Work on dashboard frontend

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -4,46 +4,54 @@ import { ResponseData } from "@/services/apiTypes";
 // import { useEffect } from "react";
 import authenticationService from "@/services/authentication-service";
 import storageService from "@/services/storage-service";
-
-
-const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-	event.preventDefault();
-  
-	const formData = new FormData(event.target as HTMLFormElement);
-  
-	const email = formData.get("email") as string | null;
-	const password = formData.get("password") as string | null;
-  
-	if (email && password) {
-	  try {
-		const response = await authenticationService.makeLoginRequest(email, password);
-		const responseData: ResponseData = response.data as ResponseData;
-  
-		// Save tokens and expiration
-		storageService.saveAccessToken(responseData.accessToken);
-		storageService.saveRefreshToken(responseData.refreshToken);
-		storageService.saveTokenExpiresDate(responseData.expiration);
-  
-		// Redirect to dashboard
-		window.location.href = "/";
-	  } catch (error: any) {
-		// Display error message using alert
-		const message =
-		  error.response?.data?.message ||
-		  error.response?.data?.errors?.[Object.keys(error.response?.data.errors)[0]] ||
-		  "An unexpected error occurred";
-  
-		alert(`Verification error: ${message}`);
-
-	  }
-	} else {
-	  alert("Please provide both email and password.");
-	}
-  };
-  
-
+import { useState } from "react";
 function Login() {
-  return (
+	const [isLoading, setIsLoading] = useState(false);
+	const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		setIsLoading(true);
+
+		const formData = new FormData(event.target as HTMLFormElement);
+
+		const email = formData.get("email") as string | null;
+		const password = formData.get("password") as string | null;
+
+		if (email && password) {
+			try {
+				const response = await authenticationService.makeLoginRequest(
+					email,
+					password,
+				);
+				const responseData: ResponseData = response.data as ResponseData;
+
+				// Save tokens and expiration
+				storageService.saveAccessToken(responseData.accessToken);
+				storageService.saveRefreshToken(responseData.refreshToken);
+				storageService.saveTokenExpiresDate(responseData.expiration);
+
+				
+				// Redirect to dashboard
+				window.location.href = "/";
+			} catch (error: any) {
+				// Display error message using alert
+				const message =
+					error.response?.data?.message ||
+					error.response?.data?.errors?.[
+						Object.keys(error.response?.data.errors)[0]
+					] ||
+					"An unexpected error occurred";
+
+				alert(`Verification error: ${message}`);
+			} finally {
+				// Always reset loading state in both success and failure
+				setIsLoading(false);
+			}
+		} else {
+			alert("Please provide both email and password.");
+		}
+	};
+
+	return (
 		<main className="w-screen h-screen bg-[#FAF5F1]">
 			<section className="w-full h-full grid grid-cols-2">
 				<div className="w-full h-full flex justify-center items-center">
@@ -91,7 +99,7 @@ function Login() {
 								type="submit"
 								className="w-full py-2 bg-[#AD3520] text-white font-semibold rounded-lg hover:bg-[#AD3520]/90 transition-all font-cabinet"
 							>
-								Login
+								{isLoading ? "Loading..." : "Login"}
 							</button>
 						</div>
 
@@ -121,4 +129,4 @@ function Login() {
 	);
 }
 
-export default Login
+export default Login;
